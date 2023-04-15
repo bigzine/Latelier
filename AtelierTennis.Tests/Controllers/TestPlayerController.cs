0d01c39d789090ca0e33ca3e9a539506c50a669e
bigzine <bigzine@gmail.com>
2023-04-15 12:11:41 +0200

    Create Project

---
+++ AtelierTennis.Tests/Controllers/TestPlayerController.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtelierTennis.Tests.Controllers
+{
+    public  class TestPlayerController
+    {
+        [Test]
+        public void Get_OnSuccess_ReturnsStatusCode200()
+        {
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Latelier
+## Run the project
+1. Clone this repository
+2. Build the solution using Visual Studio, or on the command line with dotnet build.
+3. Run the project.
